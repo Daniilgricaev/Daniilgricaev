339839e5253d331a858b48a45aae4e2d640a6f58
Daniilgricaev <154063616+Daniilgricaev@users.noreply.github.com>
2023-12-25 17:38:32 +0300

    Add files via upload

---
+++ калькулятор/калькулятор/калькулятор.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	float num1, num2,summ;
+	std::cout << "Введите первое значение :", std::cin >> num1;
+
+	std::cout << "Введите второе значение :", std::cin >> num2;
+
+	char math;
+	std::cout << "Введите символ  :", std::cin >> math;
+	if (math == '+')
+		summ = num1 + num2;
+
+	else if (math == '-')
+		summ = num1 - num2;
+
+	else if (math == '*')
+		summ = num1 * num2;
+
+	else if (math == '/')
+		summ = num1 / num2;
+
+
+	std::cout << "Ответ :" << summ;
+
+	return 0;
+}
